a71652a8309301beca3726907df26bff746318f5
jsallred-ksu <jsallred@ksu.edu>
2024-10-25 11:01:21 -0500

    i did it

--- Basic3DExample/Game1.cs
+++ Basic3DExample/Game1.cs
@@ -8,6 +8,12 @@ namespace Basic3DExample
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        // The triangle to draw
+        Triangle triangle;
+        // The quad to draw
+        Quad quad;
+        // The cube to draw 
+        Cube cube;
 
         public Game1()
         {
@@ -28,6 +34,12 @@ namespace Basic3DExample
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            // Create the triangle
+            triangle = new Triangle(this);
+            // Create the quad
+            quad = new Quad(this);
+            // Create the cube
+            cube = new Cube(this);
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +48,10 @@ namespace Basic3DExample
                 Exit();
 
             // TODO: Add your update logic here
+            // Update the triangle 
+            triangle.Update(gameTime);
+            // update the cube 
+            cube.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -45,6 +61,12 @@ namespace Basic3DExample
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            // Draw the triangle 
+            triangle.Draw();
+            // Draw the quad
+            quad.Draw();
+            // draw the cube
+            cube.Draw();
 
             base.Draw(gameTime);
         }
